c4e7a2bab10d034a87fa3b8c7fa0971b243a6439
joshDev27 <jorshua.fray@utp.ac.pa>
2024-08-20 19:48:00 -0500

    Ejercicio de perfil de usuario completado

---
+++ perfil_usuario.php
@@ -0,0 +1,16 @@
+<?php
+$nombre_completo = "Jorshua Fray";
+$edad = 20;
+$correo = "jorshua.fray@utp.ac.pa";
+$telefono= 60115422;
+
+define("OCUPACION", "¡Estudiante!");
+
+printf("Hola! mi nombre es %s y mi edad es %d <br>",$nombre_completo,$edad);
+echo "Actuamente me desempeño como ".OCUPACION. " puedes contactarme a traves de mi telefono $telefono o a mi correo $correo <br>";
+
+var_dump($nombre_completo,$edad,$correo,$telefono);
+echo "<br>";
+
+
+?>
