5d1b2a2c93b41988946f072aa10897cc18ade85e
joshDev27 <jorshua.fray@utp.ac.pa>
2024-08-20 19:11:31 -0500

    Definidas variables de diferentes tipos

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
+$nombre = "jorshua";
+$edad = 20;
+$altura = 1.75;
+$esEstudiante = true;
 
+echo "Nombre: $nombre<br>";
+echo "Edad: $edad<br>";
+echo "Altura: $altura<br>";
+echo "¿Es estudiante? " . ($esEstudiante ? "Sí" : "No");
 ?>
