733214fa317a013a227329d1d295c00a6825d524
joshDev27 <162900975+joshDev27@users.noreply.github.com>
2024-08-20 19:53:23 -0500

    agregando metodo de impresion

--- perfil_usuario.php
+++ perfil_usuario.php
@@ -8,7 +8,7 @@ define("OCUPACION", "¡Estudiante!");
 
 printf("Hola! mi nombre es %s y mi edad es %d <br>",$nombre_completo,$edad);
 echo "Actuamente me desempeño como ".OCUPACION. " puedes contactarme a traves de mi telefono $telefono o a mi correo $correo <br>";
-
+print "Saludos!"
 var_dump($nombre_completo,$edad,$correo,$telefono);
 echo "<br>";
 
