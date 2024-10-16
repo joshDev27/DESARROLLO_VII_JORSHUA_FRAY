6f053a4b1613a2e69e9143aafef4b755816088f0
joshDev27 <jorshua.fray@utp.ac.pa>
2024-10-16 05:40:46 -0500

    TALLER_8 - Paso 1: Configuración inicial de la base de datos con MySQLi y PDO

---
+++ TALLERES/TALLER_8/config_pdo.php
@@ -0,0 +1,15 @@
+
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'Panama_BD');
+define('DB_PASSWORD', 'RDP507$%&!X');
+define('DB_NAME', 'taller8_db');
+
+
+try{
+    $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e){
+    die("ERROR: No se pudo conectar. " . $e->getMessage());
+}
+?>
